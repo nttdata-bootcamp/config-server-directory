80e26bb45dc1f0a1ec4b3f79181e8f8954370cc6
jhonatan-maza <jonathan.mazav31@gmail.com>
2022-12-07 21:44:40 -0500

    update

---
+++ src/test/java/bootcamp/microservicios/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bootcamp.microservicios.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
